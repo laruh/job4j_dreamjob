51e67c319bb5669ee8ad8d2cc1510da78773ee53
laruh <oblomoff616@gmail.com>
2022-03-17 21:23:45 +0700

    3. Bootstrap, Table [#504840]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexControl {
 
-    @GetMapping("/index")
+    @GetMapping("/dreamjob")
     public String index() {
         return "index";
     }
